dc0b45d900101910525fe213f40c1ef32dc19d3c
aguedamarion <81313892+aguedamarion@users.noreply.github.com>
2022-11-21 18:38:40 -0300

    Create url-retriever.py

---
+++ url-retriever.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import urllib.request, urllib.parse, urllib.error
+import ssl
+import re
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+url = "http://py4e-data.dr-chuck.net/known_by_Hassanali.html"
+
+#to repeat 7 times
+for i in range(7):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    count = 0
+    for tag in tags:
+        count = count +1
+   
+        #make it stop at position 3
+        if count>18:
+            break
+        url = tag.get('href', None)
+
+print(url)
