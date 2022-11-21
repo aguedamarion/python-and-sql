f9c78f30ae35e1c6d08428d25c2ab92a5ac133a8
aguedamarion <81313892+aguedamarion@users.noreply.github.com>
2022-11-21 18:42:29 -0300

    Update url-retriever.py

--- url-retriever.py
+++ url-retriever.py
@@ -8,16 +8,18 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 url = "http://py4e-data.dr-chuck.net/known_by_Hassanali.html"
 
-#to repeat 7 times
+# repeats it 7 times
 for i in range(7):
+    # retrieves the url
     html = urllib.request.urlopen(url, context=ctx).read()
     soup = BeautifulSoup(html, 'html.parser')
     tags = soup('a')
+    # counts the anchors
     count = 0
     for tag in tags:
         count = count +1
    
-        #make it stop at position 3
+        # stops at position 3
         if count>18:
             break
         url = tag.get('href', None)
